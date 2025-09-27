41afe449dea86585040a2e69072e99bb2125c50c
lamooon <111581657+lamooon@users.noreply.github.com>
2025-09-28 00:43:06 +0800

    fixed CORS origins to allow any hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,4 +68,4 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = True
