bd6c6e11a5f02bdedb6d65cbc608aa46cd750309
lamooon <111581657+lamooon@users.noreply.github.com>
2025-09-28 04:11:37 +0800

    .:

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,8 +58,13 @@ TEMPLATES = [
     },
 ]
 WSGI_APPLICATION = 'backend.wsgi.application'
-# ORM 제거
-DATABASES = {}
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
