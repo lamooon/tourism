e03ab141b92d696abb18226eb49549149252672f
lamooon <111581657+lamooon@users.noreply.github.com>
2025-09-28 04:17:59 +0800

    d

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -7,9 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print("✅ WSGI Loaded successfully", file=sys.stderr)
+except Exception as e:
+    print(f"❌ WSGI failed: {e}", file=sys.stderr)
+    raise
