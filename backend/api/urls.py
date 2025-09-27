c56cb8da993321d08e992724f4f79eb4b07750fb
lamooon <111581657+lamooon@users.noreply.github.com>
2025-09-28 04:46:11 +0800

    st

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("trips/", views.TripListCreateView.as_view(), name="trip-list-create"),
     path("trips/<str:id>/", views.TripDetailView.as_view(), name="trip-detail"),
     path("rules/", views.RulesView.as_view(), name="rules"),
-    path("application/<str:id>/checklist", views.getChecklist, name="checklist"),
-    path("application/<str:id>/autofill/export", views.exportUserData, name="export-data"),
+    path("application/<str:id>/checklist/", views.getChecklist, name="checklist"),
+    path("application/<str:id>/autofill/export/", views.exportUserData, name="export-data"),
 ]
