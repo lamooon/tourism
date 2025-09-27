809ff93760c656c6c0ccaa9cacde6f6099cc85ce
lamooon <111581657+lamooon@users.noreply.github.com>
2025-09-28 03:03:32 +0800

    added gunicorn

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def root(request):
+    return JsonResponse({"message": "Backend alive"})
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", root),                     # <--- add this
+    path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
